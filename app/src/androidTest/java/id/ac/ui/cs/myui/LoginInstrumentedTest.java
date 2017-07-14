d18b0239aca3d7b40a8aba1d5f8b639bf7089095
Hafiyyan Sayyid Fadhlillah <hafiyyan94@gmail.com>
2017-07-14 18:57:44 +0700

    Add some new setting to CI and new Test Case in Instrumental Testing

--- app/src/androidTest/java/id/ac/ui/cs/myui/LoginInstrumentedTest.java
+++ app/src/androidTest/java/id/ac/ui/cs/myui/LoginInstrumentedTest.java
@@ -15,6 +15,7 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
@@ -40,5 +41,11 @@ public class LoginInstrumentedTest {
     public void labelUsername_inLoginActivity() {
         // Type text and then press the button.
         onView(withId(R.id.login)).check(matches(withText(mButtonTextExpected)));
+    }
+
+    @Test
+    public void clickLogin_showHomeActivity(){
+        onView(withId(R.id.login)).perform(click());
+        onView(withId(R.id.bottomBar)).check(matches(isDisplayed()));
     }
 }
