9514259cdfb075fb7eee1d05d31731091e0c27b4
meonster <meonster.yi@gmail.com>
2017-07-17 14:47:05 +0700

    Design layout for news detail - (haryo yusuf)

---
+++ app/src/main/java/id/ac/ui/cs/myui/activity/NewsDetailActivity.java
@@ -0,0 +1,15 @@
+package id.ac.ui.cs.myui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import id.ac.ui.cs.myui.R;
+
+public class NewsDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_detail);
+    }
+}
