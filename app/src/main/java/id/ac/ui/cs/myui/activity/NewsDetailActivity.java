726f9700987565e5fa2df3e64ec55215b854df52
meonster <meonster.yi@gmail.com>
2017-07-18 09:51:10 +0700

    penambahan string

--- app/src/main/java/id/ac/ui/cs/myui/activity/NewsDetailActivity.java
+++ app/src/main/java/id/ac/ui/cs/myui/activity/NewsDetailActivity.java
@@ -2,6 +2,7 @@ package id.ac.ui.cs.myui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import id.ac.ui.cs.myui.R;
 
@@ -11,5 +12,12 @@ public class NewsDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_detail);
+
+        TextView pubDate = (TextView) findViewById(R.id.pubdate);
+        TextView news_title = (TextView) findViewById(R.id.news_title);
+        TextView news_author = (TextView) findViewById(R.id.news_author);
+        TextView news_content = (TextView) findViewById(R.id.news_content);
+
+
     }
 }
