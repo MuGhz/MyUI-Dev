13e5a7ecfe77be4cf316b5235754b0811474b41b
muhammad ghozi <ghozi.mghozi@gmail.com>
2017-07-19 11:45:52 +0700

    share via line with snippet - ghozi irma

--- app/src/main/java/id/ac/ui/cs/myui/activity/NewsDetailActivity.java
+++ app/src/main/java/id/ac/ui/cs/myui/activity/NewsDetailActivity.java
@@ -1,33 +1,49 @@
 package id.ac.ui.cs.myui.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.List;
 
 import id.ac.ui.cs.myui.R;
 
 public class NewsDetailActivity extends AppCompatActivity {
+    public static final String PACKAGE_NAME = "jp.naver.line.android";
+    public static final String CLASS_NAME = "jp.naver.line.android.activity.selectchat.SelectChatActivity";
+    Context context;
+    ImageButton ibShareLine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_detail);
-
+        context=this;
         Intent intent = getIntent();
+
+        final String link = intent.getStringExtra("link");
+
+
+        ibShareLine = (ImageButton) findViewById(R.id.button_line);
 
         TextView pubDate = (TextView) findViewById(R.id.pubdate);
         pubDate.setText(intent.getStringExtra("pubDate"));
 
-        TextView news_title = (TextView) findViewById(R.id.news_title);
+        final TextView news_title = (TextView) findViewById(R.id.news_title);
         news_title.setText(intent.getStringExtra("title"));
 
-        TextView news_author = (TextView) findViewById(R.id.news_author);
+        final TextView news_author = (TextView) findViewById(R.id.news_author);
         news_author.setText(intent.getStringExtra("author"));
         news_author.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,7 +52,7 @@ public class NewsDetailActivity extends AppCompatActivity {
             }
         });
 
-        TextView news_content = (TextView) findViewById(R.id.news_content);
+        final TextView news_content = (TextView) findViewById(R.id.news_content);
         if (Build.VERSION.SDK_INT >= 24) {
             news_content.setText(Html.fromHtml(intent.getStringExtra("content"),Html.FROM_HTML_MODE_LEGACY));
         }
@@ -44,6 +60,49 @@ public class NewsDetailActivity extends AppCompatActivity {
             news_content.setText(Html.fromHtml(intent.getStringExtra("content")));
         }
 
+        String[] tmp = news_content.getText().toString().split(" ");
+        String sendString="";
+        if(tmp.length>=25) {
+            for (int i = 0; i < 25; i++) {
+                sendString += tmp[i] + " ";
+            }
+        }else{
+            sendString=news_content.getText().toString()
+            ;
+        }
+        final String snippet=sendString+"...";
+        ibShareLine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkLineInstalled()) {
+                    Intent intent = new Intent(Intent.ACTION_SEND);
+                    intent.setClassName(PACKAGE_NAME, CLASS_NAME);
+                    intent.setType("text/plain");
+                    String shareBodyText = news_title.getText() + "\n" + news_author.getText()
+                            + "\n" + snippet + "\n\nSelengkapnya : \n" + link;
+                    intent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject/Title");
+                    intent.putExtra(android.content.Intent.EXTRA_TEXT, shareBodyText);
+                    //startActivity(Intent.createChooser(intent, "Choose sharing method"));
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(context, "LINE tidak terdeteksi, silahkan install terlebih dahulu", Toast.LENGTH_SHORT).show();
+                }
+            }
 
+        });
+
+    }
+
+    private boolean checkLineInstalled(){
+        PackageManager pm = getPackageManager();
+        List<ApplicationInfo> m_appList = pm.getInstalledApplications(0);
+        boolean lineInstallFlag = false;
+        for (ApplicationInfo ai : m_appList) {
+            if(ai.packageName.equals(PACKAGE_NAME)){
+                lineInstallFlag = true;
+                break;
+            }
+        }
+        return lineInstallFlag;
     }
 }
