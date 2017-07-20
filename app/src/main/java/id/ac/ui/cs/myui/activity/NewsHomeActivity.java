42ff50f26d18880c469bdd007e611fca93d8a18c
ekanaradhipad <ekanaradhipad@gmail.com>
2017-07-20 15:29:50 +0700

    configure crashlytic

--- app/src/main/java/id/ac/ui/cs/myui/activity/NewsHomeActivity.java
+++ app/src/main/java/id/ac/ui/cs/myui/activity/NewsHomeActivity.java
@@ -5,14 +5,17 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.crashlytics.android.Crashlytics;
 import id.ac.ui.cs.myui.R;
 import id.ac.ui.cs.myui.task.NewsSceleTask;
+import io.fabric.sdk.android.Fabric;
 
 public class NewsHomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_news_home);
 
         new NewsSceleTask(this).execute();
